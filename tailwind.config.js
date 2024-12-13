4a5b6e74ea580374686dbba3630d65abc65d36b7
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-12-13 16:22:28 +0300

    change tailwindconfig && add apply styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FFA62B',
+        secondary: '#16697A',
+        accent: '#00A1C1',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
