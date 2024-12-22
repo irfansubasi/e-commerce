57627bd823b02dfd8920e48dbc6fc7fd352ee177
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-12-22 18:17:06 +0300

    fix pickCard padding

--- src/components/PickCard.jsx
+++ src/components/PickCard.jsx
@@ -4,7 +4,7 @@ export default function PickCard({ category, imgSrc }) {
       style={{ backgroundImage: `url(${imgSrc})` }}
       className={`bg-no-repeat bg-center bg-cover relative w-full h-full`}
     >
-      <h5 className="absolute bottom-5 left-5 bg-white text-txt px-10 py-2">
+      <h5 className="absolute bottom-5 left-5 bg-white text-txt px-5 lg:px-10 py-2">
         {category}
       </h5>
     </div>
